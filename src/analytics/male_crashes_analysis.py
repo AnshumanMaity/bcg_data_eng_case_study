33eb95270d21dfc1f54b0d84a6c01e7f31ef45bc
AnshMaity <ansumanmaity10000@gmail.com>
2024-09-03 18:10:13 +0530

    updated analysis logic

--- src/analytics/male_crashes_analysis.py
+++ src/analytics/male_crashes_analysis.py
@@ -17,13 +17,16 @@ class MaleCrashesAnalysis:
         # Loads input files path into variables
         source_path = files['inputpath']
         person_use_csv_path = source_path + "/" + files["person"]
+        units_use_csv_path = source_path + "/" + files["units"]
 
         # Loads the primary person data into df
         primary_person_df = Utils.load_csv(session=session, path=person_use_csv_path, header=True,
                                    schema=schemas.primary_person_schema)
+        units_df = Utils.load_csv(session=session, path=units_use_csv_path, header=True,
+                                  schema=schemas.units_schema)
 
         #Calculates the no of accidents where males killed are greater than 2
-        total_crashes_males = primary_person_df.where((col("PRSN_GNDR_ID") == 'MALE') & (col("DEATH_CNT") > 2)).count()
+        total_crashes_males = primary_person_df.join(units_df,['CRASH_ID','UNIT_NBR'], how='inner').filter((primary_person_df.PRSN_GNDR_ID == 'MALE') & (units_df.DEATH_CNT > 2)).select('CRASH_ID').distinct().count()
 
         #Returns the result in a dataframe
         return session.createDataFrame([("Analytics 1: Find the number of crashes (accidents) in which number of males killed are greater than 2?",total_crashes_males)],'a string, b long')
